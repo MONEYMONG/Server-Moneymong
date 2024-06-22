74433d7ff20b4f65220e3b3fcc122027ff31d41f
rlarltj123 <rlarltj1234@gmail.com>
2024-06-22 16:17:05 +0900

    feat: totalCount를 포함하는 LedgerInfoViewV2를 추가한다.

---
+++ src/main/java/com/moneymong/domain/ledger/api/response/ledger/LedgerInfoViewV2.java
@@ -0,0 +1,31 @@
+package com.moneymong.domain.ledger.api.response.ledger;
+
+import com.moneymong.domain.ledger.entity.Ledger;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@Builder
+@RequiredArgsConstructor
+public class LedgerInfoViewV2 {
+    final Long id;
+    final Integer totalBalance;
+    final List<LedgerInfoViewDetail> ledgerInfoViewDetails;
+    final long ledgerDetailTotalCount;
+
+    public static LedgerInfoViewV2 from(
+            Ledger ledger,
+            List<LedgerInfoViewDetail> ledgerInfoViewDetails,
+            long ledgerDetailTotalCount
+    ) {
+        return LedgerInfoViewV2.builder()
+                .id(ledger.getId())
+                .totalBalance(ledger.getTotalBalance())
+                .ledgerInfoViewDetails(ledgerInfoViewDetails)
+                .ledgerDetailTotalCount(ledgerDetailTotalCount)
+                .build();
+    }
+}
